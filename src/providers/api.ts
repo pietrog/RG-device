3bbbb79800d6eb1836cfee3e6f750f605a4738e8
Pierre Gaulard <pierre.gaulard@gmail.com>
2018-03-19 23:15:58 +0100

    Update package json

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -46,12 +46,12 @@ export class Api {
     ) {
 	//this.ip_address = '192.168.1.109'; //ip address rg camp
 	//this.ip_address = '192.168.0.21';
-	this.ip_address = '10.0.0.7';
-	//this.ip_address = '0.0.0.0';
+	//this.ip_address = '10.0.0.7';
+	this.ip_address = '0.0.0.0';
 	this.port = "3000";
 
-	this.url = `http://${this.ip_address}:${this.port}`;
-	//this.url = ''; //needed when server or run debug
+	//this.url = `http://${this.ip_address}:${this.port}`;
+	this.url = ''; //needed when server or run debug
 	this._socket = io(this.url);
 
 	const url = `${this.url}/api/device/idFromName`;
